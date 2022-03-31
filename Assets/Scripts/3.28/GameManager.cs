9b8ae7066121860ea92fb714253a5fc4ddfd4cf9
jessi68 <lyn990312@gmail.com>
2022-03-31 23:43:53 +0900

    fix: fix error when merge with 3.30 assignment

--- Assets/Scripts/3.28/GameManager.cs
+++ Assets/Scripts/3.28/GameManager.cs
@@ -4,84 +4,97 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour, Subject
 {
+    // 1. Singleton Pattern: Instance() method
+    
     private static GameManager _instance;
+
     public static GameManager Instance()
     {
         return _instance;
     }
 
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else
-        {
-            if (this != _instance)
-            {
-                Destroy(this.gameObject);
-            }
-        }
-    }
-
+    // 초기화 설정 바꾸지 말 것
     private int _gameRound = 0;
     private string _whoseTurn = "Enemy";
     private bool _isEnd = false;
 
-    // 1. SceneUI가 GameManager 접근 할 수 있도록 캐릭터 딕셔너리 선언
-    private Dictionary<string, Character> _characterList = new Dictionary<string, Character>();
+    // delegate: TurnHandler, FinishHandler 선언
+    delegate void TurnHandler(int round, string turn);
+    delegate void FinishHandler(bool isFinish);
 
-    private delegate void TurnHandler(int round, string turn);
-    private TurnHandler _turnHandler;
-    private delegate void FinishHandler(bool isFinish);
-    private FinishHandler _finishHandler;
-    // 2. UIHandler 선언 (이번에는 round, turn, isFinish 모두 받는다)
-    private delegate void UIHandler(int round, string turn, bool isFinish);
-    private UIHandler _uiHandler;
+    TurnHandler _turnHandler;
+    FinishHandler _finishHandler;
 
+    void Start()
+    {
+      
+        
+    }
+    /// <summary>
+    /// 2. RoundNotify:
+    /// 1) 현재 턴이 Enemy이면 다음 gameRound로
+    ///  + Debug.Log($"GameManager: Round {gameRound}.");
+    /// 2) TurnNotify() 호출
+    /// </summary>
     public void RoundNotify()
     {
-        if (!_isEnd)
+        if(_whoseTurn == "Enemy")
         {
-            if(_whoseTurn == "Enemy")
-            {
-                _gameRound++;
-                Debug.Log($"GameManager: Round {_gameRound}.");
-            }
-            TurnNotify();
+            _gameRound += 1;
+            Debug.Log($"GameManager: Round {_gameRound}.");
         }
+        TurnNotify();
     }
 
+    /// <summary>
+    /// 3. TurnNotify:
+    /// 1) whoseTurn update
+    ///  + Debug.Log($"GameManager: {_whoseTurn} turn.");
+    /// 2) _turnHandler 호출
+    /// </summary>
     public void TurnNotify()
     {
-        _whoseTurn = _whoseTurn == "Enemy" ? "Player" : "Enemy";
-        Debug.Log($"GameManager: {_whoseTurn} turn.");
+       if(_whoseTurn == "Enemy")
+        {
+            _whoseTurn = "Player";
+        } else
+        {
+            _whoseTurn = "Enemy";
+        }
+        
         _turnHandler(_gameRound, _whoseTurn);
-        // 2. _uiHandler 호출
+        Debug.Log($"GameManager: {_whoseTurn} turn.");
+
+        
     }
 
+    /// <summary>
+    /// 4. EndNotify: 
+    /// 1) isEnd update
+    ///  + Debug.Log("GameManager: The End");
+    ///  + Debug.Log($"GameManager: {_whoseTurn} is Win!");
+    /// 2) _finishHandler 호출
+    /// </summary>
     public void EndNotify()
     {
         _isEnd = true;
-        _finishHandler(_isEnd);
-        // 2. _uiHandler 호출
         Debug.Log("GameManager: The End");
         Debug.Log($"GameManager: {_whoseTurn} is Win!");
+        _finishHandler(_isEnd);
+
     }
 
+    // 5. AddCharacter: _turnHandler, _finishHandler 각각에 메소드 추가
     public void AddCharacter(Character character)
     {
-        _turnHandler += new TurnHandler(character.TurnUpdate);
-        _finishHandler += new FinishHandler(character.FinishUpdate);
-        // 1. _characterList에 추가
+        _turnHandler += new TurnHandler(character.GetComponent<Character>().TurnUpdate);
+        _finishHandler += new FinishHandler(character.GetComponent<Character>().FinishUpdate);
     }
 
     // 3. AddUI: SceneUI 옵저버로 등록
     public void AddUI(SceneUI ui)
     {
-    
+
     }
 
     /// <summary>
--- Assets/Scripts/3.28/Player.cs
+++ Assets/Scripts/3.28/Player.cs
@@ -4,47 +4,75 @@ using UnityEngine;
 
 public class Player : Character
 {
-    // 1. _enemy 변수 삭제 -> GetCharacter로 접근할 거임
-    //private Enemy _enemy;
+    private Enemy _enemy;
     private float _randomAttack;
+    private int SPECIAL_ATTACK_MAX_VALUE = 3;
+    private int SPECIAL_ATTACK_ADDITIOANL_VALUE = 10;
 
+    /// <summary>
+    /// 1. Init: 초기화 기능
+    /// 1) Subject에 Observer로 등록
+    /// 2) _myName, _myHp, _myDamage 초기화
+    /// 3) _myName은 무조건 "Player"로 할 것
+    /// 4) _myHp, _myDamage는 100, 20으로 각각 초기화 (권장 사항)
+    /// </summary>
     protected override void Init()
     {
         base.Init();
         _myName = "Player";
-        _myHpMax = 100;
-        _myHp = _myHpMax;
+        _myHp = 100;
         _myDamage = 20;
-        GameManager.Instance().AddCharacter(this.GetComponent<Player>());
     }
 
+    /// <summary>
+    /// 1) _enemy가 할당이 안됐다면,
+    /// 2) GameObject.FindWithTag 이용해서 _enemy 할당
+    /// </summary>
     private void Awake()
     {
         Init();
+        if (_enemy == null)
+        {
+            _enemy = GameObject.FindWithTag("Enemy").GetComponent<Enemy>();
+        }
     }
 
+    /// <summary>
+    /// Attack:
+    /// 1) Player는 30%의 확률로 공격력이 더 높은 공격을 가할 것
+    /// 2) _randomAttack = Random.Range(0,10); 으로 랜덤 변수 생성
+    ///   -> 0~9 까지의 정수 중 하나를 랜덤으로 할당받음.
+    /// 3) _randomAttack 이용해서 30% 확률로 기존 공격력보다 10 높은 공격 실행
+    /// 4) 이때는 AttackMotion() 말고 SpecialAttackMotion() 호출할 것
+    ///    + Debug.Log($"{_myName} Special Attack!"); 추가
+    /// 5) 70% 확률로 하는 일반 공격은 Character에 써있는 주석과 동일
+    /// </summary>
     public override void Attack()
     {
-        if(_myName.Equals(_whoseTurn) && !_isFinished)
+        float _damage = 0;
+
+        Debug.Log("Attack");
+        if (!_isFinished && _myName == _whoseTurn)
         {
-            _randomAttack = Random.Range(0, 10);
-            if (_randomAttack < 7)
-            {
-                AttackMotion();
-                // 1. GetCharacter로 Enemy 접근
-                GameManager.Instance().GetCharacter("Enemy").GetHit(_myDamage);
-            }
-            else
+            int _randomAttack = Random.Range(0, 10);
+            if(0 <= _randomAttack && _randomAttack < SPECIAL_ATTACK_MAX_VALUE)
             {
                 SpecialAttackMotion();
+                _damage = _myDamage + SPECIAL_ATTACK_ADDITIOANL_VALUE;
                 Debug.Log($"{_myName} Special Attack!");
-                GameManager.Instance().GetCharacter("Enemy").GetHit(_myDamage + 10);
+            } else
+            {
+                AttackMotion();
+                _damage = _myDamage;
             }
+
+            _enemy.GetHit(_damage);
         }
     }
 
     public override void GetHit(float damage)
     {
         base.GetHit(damage);
+
     }
 }
