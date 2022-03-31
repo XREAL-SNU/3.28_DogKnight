c4cedc3024a9fc486560ec750cb3ff592a9db59c
lena1113 <102497168+aNelhwc@users.noreply.github.com>
2022-03-31 19:48:44 +0900

    (feat) add TurnNotify

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,38 +12,50 @@ public class GameManager : MonoBehaviour, Subject
     }
     
 
-    // �ʱ�ȭ ���� �ٲ��� �� ��
+    // 초기화 설정 바꾸지 말 것
     private int _gameRound = 0;
     private string _whoseTurn = "Enemy";
     private bool _isEnd = false;
 
-    // delegate: TurnHandler, FinishHandler ����
+    // delegate: TurnHandler, FinishHandler 선언
     delegate void TurnUpdate(int round, string turn);
-    TurnHandler _turnHandler
+    TurnUpdate _turnHandler;
 
-    delegate void FinishUpdate(boo isFinish);
-    FinishHandler _finishHandler
+    delegate void FinishUpdate(bool isFinish);
+    FinishUpdate _finishHandler;
 
     /// <summary>
     /// 2. RoundNotify:
-    /// 1) ���� ���� Enemy�̸� ���� gameRound��
+    /// 1) 현재 턴이 Enemy이면 다음 gameRound로
     ///  + Debug.Log($"GameManager: Round {gameRound}.");
-    /// 2) TurnNotify() ȣ��
+    /// 2) TurnNotify() 호출
     /// </summary>
     public void RoundNotify()
     {
-
+        _whoseTurn = "Enemy";
+        if(_whoseTurn == "Enemy")
+        {
+            _gameRound = _gameRound + 1;
+            Debug.Log($"GameManager: Round {_gameRound}.");
+            TurnNotify();
+        }
+        
     }
 
     /// <summary>
     /// 3. TurnNotify:
     /// 1) whoseTurn update
     ///  + Debug.Log($"GameManager: {_whoseTurn} turn.");
-    /// 2) _turnHandler ȣ��
+    /// 2) _turnHandler 호출
     /// </summary>
     public void TurnNotify()
     {
-
+        _whoseTurn= "Player";
+        
+        Debug.Log($"GameManager: {_whoseTurn} turn.");
+            
+        RoundNotify();
+        //_turnHandler(_gameRound, _whoseTurn);
     }
 
     /// <summary>
@@ -51,11 +63,14 @@ public class GameManager : MonoBehaviour, Subject
     /// 1) isEnd update
     ///  + Debug.Log("GameManager: The End");
     ///  + Debug.Log($"GameManager: {_whoseTurn} is Win!");
-    /// 2) _finishHandler ȣ��
+    /// 2) _finishHandler 호출
     /// </summary>
     public void EndNotify()
     {
-
+        _isEnd = true;
+        Debug.Log("GameManager: The End");
+        Debug.Log($"GameManager: {_whoseTurn} is Win!");
+        _finishHandler(_isEnd);
     }
 
     // 5. AddCharacter: _turnHandler, _finishHandler ������ �޼ҵ� �߰�
