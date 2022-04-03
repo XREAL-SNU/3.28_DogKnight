2592fe5f0df50b5e11fb2156b8da6691a96868ca
Hyoin Jeong <hobbyhyoin@naver.com>
2022-04-04 01:33:00 +0900

    [feat] SceneUI End
    End of SceneUI (gameround,hp,attackbutton)

---
+++ Assets/Scripts/DogKnight/Game/Character.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 애니메이팅 트리거 이름 열거형으로 저장 (이해할 필요 없음)
+public enum AnimatorParameters
+{
+    IsAttack, IsSpecialAttack, GetHit, IsDead
+}
+
+public class Character : MonoBehaviour, Observer
+{
+    public string _myName;
+    public float _myHp;
+    // 1. Hp UI bar 구현을 위해 HpMax 값 추가
+    public float _myHpMax;
+    public float _myDamage;
+
+    protected int _gameRound;
+    protected string _whoseTurn;
+    protected bool _isFinished;
+
+    public void TurnUpdate(int round, string turn)
+    {
+        this._gameRound = round;
+        this._whoseTurn = turn;
+    }
+
+    public void FinishUpdate(bool isFinish)
+    {
+        this._isFinished = isFinish;
+    }
+
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public virtual void GetHit(float damage)
+    {
+        _myHp -= damage;
+        if (_myHp <= 0)
+        {
+            DeadMotion();
+            GameManager.Instance().EndNotify();
+        }
+        else
+        {
+            GetHitMotion();
+            Debug.Log($"{_myName} HP: {_myHp}");
+        }
+    }
+
+    protected Animator _animator;
+
+    protected virtual void Init()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    protected void AttackMotion()
+    {
+        _animator.SetTrigger(AnimatorParameters.IsAttack.ToString());
+    }
+    protected void SpecialAttackMotion()
+    {
+        _animator.SetTrigger(AnimatorParameters.IsSpecialAttack.ToString());
+    }
+
+    protected void DeadMotion()
+    {
+        StartCoroutine(DeadCoroutine());
+    }
+
+    protected void GetHitMotion()
+    {
+        StartCoroutine(GetHitCoroutine());
+    }
+
+    IEnumerator GetHitCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        _animator.SetTrigger(AnimatorParameters.GetHit.ToString());
+    }
+
+    IEnumerator DeadCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        _animator.SetTrigger(AnimatorParameters.IsDead.ToString());
+    }
+}
---
+++ Assets/Scripts/DogKnight/Game/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    // 1. _enemy 변수 삭제 -> GetCharacter로 접근할 거임
+    //private Enemy _enemy;
+    private float _randomAttack;
+
+    protected override void Init()
+    {
+        base.Init();
+        _myName = "Player";
+        _myHpMax = 100;
+        _myHp = _myHpMax;
+        _myDamage = 20;
+        GameManager.Instance().AddCharacter(this.GetComponent<Player>());
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public override void Attack()
+    {
+        if (_myName.Equals(_whoseTurn) && !_isFinished)
+        {
+            _randomAttack = Random.Range(0, 10);
+            if (_randomAttack < 7)
+            {
+                AttackMotion();
+                // 1. GetCharacter로 Enemy 접근
+                GameManager.Instance().GetCharacter("Enemy").GetHit(_myDamage);
+            }
+            else
+            {
+                SpecialAttackMotion();
+                Debug.Log($"{_myName} Special Attack!");
+                GameManager.Instance().GetCharacter("Enemy").GetHit(_myDamage + 10);
+            }
+        }
+    }
+
+    public override void GetHit(float damage)
+    {
+        base.GetHit(damage);
+    }
+}
