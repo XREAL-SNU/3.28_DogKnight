004bd073571ee654c8ea199e08ea98bda4c9d1fa
sibisibi <102299026+sibisibi@users.noreply.github.com>
2022-04-06 23:50:03 +0900

    [Comment] Refactor Code, Add Comment
    기존에 주석으로 함수 위에 쌓여 있던 지시사항을 수행하면서 코드 내부로 주석을 옮기며 작업 중입니다. 이 외에 코드 리팩토링 작업 중입니다.

--- Assets/Scripts/SceneUI.cs
+++ Assets/Scripts/SceneUI.cs
@@ -7,15 +7,23 @@ using XReal.XTown.UI;
 
 public class SceneUI : UIScene
 {
-    // 1. enum 자유롭게 구성
-    enum GameObjects
+    // component 종류에 따라 분류하여 enum 구성
+    enum Buttons
     {
         AttackButton,
-        InventoryButton,
-        GameRoundText
-        //CharacterHp
-        //GameEndText
-
+        InventoryButton
+    }
+    enum Texts
+    {
+        GameRoundText,
+        GameOverText
+    }
+    enum Images
+    {
+        PlayerHpBar,
+        EnemyHpBar,
+        GameOverPanel,
+        HealImage
     }
 
     // 서브젝트에게 넘겨받을 변수들
@@ -28,123 +36,124 @@ public class SceneUI : UIScene
     // Attack 버튼 이중 클릭 방지 bool 변수
     private bool _isClicked = false;
 
-
     private void Start()
     {
         Init();
 
         _player = GameManager.Instance().GetCharacter("Player");
         _enemy = GameManager.Instance().GetCharacter("Enemy");
+
+        // GameManager의 옵저버로 등록
         GameManager.Instance().AddUI(this);
-        // 1. 옵저버 등록: AddUI(this);
-        // 1. Game Ending 됐을 때 뜨는 UI 비활성화
+
+        // Start 시 뜨면 안 되는 UI 비활성화
+        GetImage((int)Images.GameOverPanel).gameObject.SetActive(false);
     }
 
-    /// <summary>
-    /// 2. Init: 각 요소에 함수 바인딩
-    /// 1) Attack Button에 OnClick_AttackButton Bind
-    /// 2) 인벤토리 창 여는 버튼에 OnClick_InventoryButton Bind
-    /// </summary>
     public override void Init()
     {
         base.Init();
 
-        Bind<GameObject>(typeof(GameObjects));
+        // 각 요소에 함수 바인딩
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+        Bind<Image>(typeof(Images));
 
-        GameObject attackButton = GetUIComponent<GameObject>((int)GameObjects.AttackButton);
-        attackButton.BindEvent(OnClick_AttackButton);
-        attackButton.BindEvent(OnButtonEnter, UIEvents.UIEvent.Enter);
+        // Attack Button에 OnClick_AttackButton Bind
+        GetButton((int)Buttons.AttackButton).gameObject.BindEvent(OnClick_AttackButton);
 
-        GameObject inventoryButton = GetUIComponent<GameObject>((int)GameObjects.InventoryButton);
-        inventoryButton.BindEvent(OnClick_InventoryButton);
-        inventoryButton.BindEvent(OnButtonEnter, UIEvents.UIEvent.Enter);
+        // 인벤토리 창 여는 버튼에 OnClick_InventoryButton Bind
+        GetButton((int)Buttons.InventoryButton).gameObject.BindEvent(OnClick_InventoryButton);
     }
 
-    /// <summary>
-    /// 3. OnClick_AttackButton
-    /// 1) 애니메이션 실행 중이 아니라면(!_isClicked)
-    /// 2) 서브젝트에게 RoundNotify로 옵저버들에게 이벤트 발행
-    /// 3) GameRoundText();
-    /// 4) 플레이어 공격 (_player.Attack)
-    /// 5) 적 공격 (_enemy.Attack)
-    /// 6) StartCoroutine(GetDamageCoroutine());
-    /// </summary>
     public void OnClick_AttackButton(PointerEventData data)
     {
+        // 애니메이션 실행 중이 아니라면,
         if (!_isClicked)
         {
+            _isClicked = true;
+
+            // 서브젝트에게 RoundNotify -> 옵저버들에게 이벤트 발행
             GameManager.Instance().RoundNotify();
-            UpdateGameRoundText();
+
+            // Update UI: Text of Game Round
+            GameRoundText();
+
             _player.Attack();
             _enemy.Attack();
+
             StartCoroutine(GetDamageCoroutine());
         }
     }
 
-    /// <summary>
-    /// 4. OnClick_InventoryButton: 
-    /// 1) Player 턴이라면
-    /// 2) Inventory UIPopup 띄우기 (ShowPopupUI)
-    /// </summary>
     public void OnClick_InventoryButton(PointerEventData data)
     {
+        // Player 턴이라면,
         if (_whoseTurn.Equals("Player"))
         {
-            // Inventory UIPopup : ShowPopupUI
+            // Inventory UIPopup 띄우기
+            UIManager.UI.ShowPopupUI<UIPopup>("Inventory");
         }
     }
 
-    // 5. GameRoundText: GameRound 띄우는 UI의 text 업데이트
-    public void UpdateGameRoundText()
+    // update UI: text of new game round
+    public void GameRoundText()
     {
-        Text gameRoundText = GetUIComponent<Text>((int)GameObjects.GameRoundText);
-        gameRoundText.text = _gameRound.ToString();
+        GetText((int)Texts.GameRoundText).text = "Game Round " + _gameRound;
     }
 
-    // 6. CharacterHp: CharacterHp UI 업데이트 -> fillAmount 값 이용
-    public void UpdateCharacterHp()
+    // Update UI: HP bar of Character
+    public void CharacterHp()
     {
-        // CharacterHp UI 업데이트 -> fillAmount 값 이용
+        GetImage((int)Images.PlayerHpBar).fillAmount = _player._myHp / _player._myHpMax;
+        GetImage((int)Images.EnemyHpBar).fillAmount = _enemy._myHp / _enemy._myHpMax;
     }
 
-    /// <summary>
-    /// 7. GameEnd:
-    /// 1) 게임이 끝났다면,
-    /// 2) GameEnd UI 활성화
-    /// 3) 이긴 캐릭터 이름 Text 업데이트
-    /// </summary>
-    public void UpdateGameEnd()
+    public void GameEnd()
     {
+        // 게임이 끝났다면,
         if (_isEnd)
         {
-            // activate GameEnd UI
-            // update 이긴 캐릭터 이름 Text
+            // activate UI: game over image
+            GetImage((int)Images.GameOverPanel).gameObject.SetActive(true);
+
+            // update UI: text including winner's name
+            GetText((int)Texts.GameOverText).text = "Game Over!!\n" + _whoseTurn + " Win!!";
         }
     }
 
     // 7. GetDamageCoroutine: 각 캐릭터들의 공격/피격 애니메이션에 맞추어 UI 표현이 자연스러울 수 있도록
     IEnumerator GetDamageCoroutine()
     {
+        GetButton((int)Buttons.AttackButton).interactable = false;
+        if (_whoseTurn.Equals("Player"))
+        {
+            GetButton((int)Buttons.InventoryButton).interactable = false;
+        }
         yield return new WaitForSeconds(1.2f);
-        UpdateCharacterHp();
+        CharacterHp();
+
+        float beforeHp = _enemy._myHp;
         yield return new WaitForSeconds(1.2f);
-        UpdateGameEnd();
-        UpdateCharacterHp();
-        _isClicked = !_isClicked;
-        // 7. 다시 버튼 눌릴 수 있도록 _isClicked 조절
+        GameEnd();
+        float afterHp = _enemy._myHp;
+        CharacterHp();
+        if (beforeHp != afterHp)
+        {
+            GetImage((int)Images.HealImage).gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.65f);
+            GetImage((int)Images.HealImage).gameObject.SetActive(false);
+        }
+        GetButton((int)Buttons.AttackButton).interactable = true;
+        if (_whoseTurn.Equals("Enemy")) GetButton((int)Buttons.InventoryButton).interactable = true;
+        _isClicked = false;
     }
 
     // 8. UIUpdate: 서브젝트 델리게이트에 등록될 옵저버 업데이트 함수 -> 변수 업데이트
     public void UIUpdate(int round, string turn, bool isFinish)
     {
-        _gameRound = round;
-        _whoseTurn = turn;
-        _isEnd = isFinish;
-        // update UI
-    }
-
-    public void OnButtonEnter(PointerEventData data)
-    {
-        Debug.Log(data.pointerEnter.name + " Enter!");
+        this._gameRound = round;
+        this._whoseTurn = turn;
+        this._isEnd = isFinish;
     }
 }
